a590615bf09440ff4fbe58ea78d6687de3bcd485
praveen <praveen.rajpal@gmail.com>
2018-07-14 09:43:58 +0530

    Added Movies

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+class Movies(models.Model):
+    director = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    movie_title = models.CharField(max_length=200)
+    synopsis = models.TextField()
+    started_on = models.DateTimeField(
+            default=timezone.now)
+    launched_on = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.launched_on = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.movie_title
